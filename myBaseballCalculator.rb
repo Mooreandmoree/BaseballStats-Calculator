2c65d2e885952a85377023f03f28c05635ea68b0
Momoh Bliss <131541515+Mooreandmoree@users.noreply.github.com>
2024-09-23 11:53:27 +0100

    Add files via upload

---
+++ myBaseballCalculator.rb
@@ -0,0 +1,20 @@
+class BaseballPlayer
+  def initialize(hits, walks, bats)
+    @hits = hits
+    @walks = walks
+    @bats = bats
+  end
+
+  def batting_average
+    @hits / @bats.to_f
+  end
+
+  def on_base_percentage
+    (@hits + @walks) / @bats.to_f
+  end
+end
+
+roger = BaseballPlayer.new(22, 4, 40)
+puts roger.batting_average
+clemens = BaseballPlayer.new(25, 3, 40)
+puts clemens.on_base_percentage
